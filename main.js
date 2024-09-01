57c28423e114da7ba23aa25dd4e1d3c016d1628b
gavkhar16 <khabibullaevagavkhar@gmail.com>
2024-09-02 03:03:28 +0500

    hard 2

---
+++ main.js
@@ -0,0 +1,21 @@
+function saleDate(){
+
+    let newDate = new Date()
+    let saleDate= new Date(2024, 10,20);
+    let finishDate =saleDate-newDate   
+    let day =Math.floor(finishDate/(1000*60*60*24))    
+    let hours = Math.floor((finishDate% (1000*60*60*24))/(1000*60*60))   
+    let minutes = Math.floor((finishDate %(1000*60*60))/(1000*60))    
+    let seconds = Math.floor((finishDate%(1000*60))/1000)    
+    let dayValue= document.getElementsByClassName("time")[0]    
+    let hoursValue= document.getElementsByClassName("time")[1]    
+    let minutesValue= document.getElementsByClassName("time")[2]    
+    let secondsValue= document.getElementsByClassName("time") [3]   
+    dayValue.innerHTML=day    
+    hoursValue.innerHTML=hours   
+    minutesValue.innerHTML=minutes    
+    secondsValue.innerHTML=seconds    
+    }    
+    setInterval(()=>{   
+    saleDate()    
+    },1000)
